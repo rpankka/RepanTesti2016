cf0f1b06c9b4578078bd12e40e121f4d5be1c0dc
pankka <pankka@PORI112-021-SPC.intra.tut.fi>
2016-03-14 10:37:06 +0200

    Toka commit

--- maananataikoodia/src/maananataikoodia/Testausta.java
+++ maananataikoodia/src/maananataikoodia/Testausta.java
@@ -5,6 +5,7 @@ public class Testausta {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ekaa committia pukkaa!");
+		System.out.println("toka committi liit‰‰!");
 
 	}
 
