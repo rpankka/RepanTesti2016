82ca36508e0a37d25ff033bb50dee1f488293b01
pankka <pankka@PORI112-021-SPC.intra.tut.fi>
2016-03-14 10:32:17 +0200

    eka commit

---
+++ maananataikoodia/src/maananataikoodia/Testausta.java
@@ -0,0 +1,11 @@
+package maananataikoodia;
+
+public class Testausta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ekaa committia pukkaa!");
+
+	}
+
+}
